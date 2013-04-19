ede0843535518dc5e21dda39da722502e184ec76
fission <surya.nukala@fissionlabs.in>
2013-04-19 14:51:43 +0530

    new

---
+++ testString/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  testString
+//
+//  Created by SUBRAHMANYAM NUKALA on 15/03/13.
+//  Copyright (c) 2013 SUBRAHMANYAM NUKALA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
