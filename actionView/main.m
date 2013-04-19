9d7f6a04171f1536b704955166f59ea2de73cb80
fission <surya.nukala@fissionlabs.in>
2013-04-19 17:38:58 +0530

    please work

---
+++ actionView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  actionView
+//
+//  Created by SUBRAHMANYAM NUKALA on 01/04/13.
+//  Copyright (c) 2013 SUBRAHMANYAM NUKALA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
